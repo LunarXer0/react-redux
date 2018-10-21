00c454f958c8c104ce489325935c437be5f93aa7
Victor Rivera <verm2405@gmail.com>
2018-10-21 12:15:03 -0400

    Actions, Reducers, Middleware, Thunk
    Also organized folder structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import logo from "./logo.svg";
 import "./App.css";
 
 import rootReducer from "./rootReducer";
-import MoviesList from "./MoviesList";
-import MovieDetail from "./MovieDetail";
 
-const store = createStore(rootReducer, {}, composeWithDevTools());
+import MoviesList from "./movies/MoviesList";
+import MovieDetail from "./movies/MovieDetail";
+import Toggle from "./toggle/Toggle";
+
+const middleware = [logger, thunk];
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 const App = () => (
   <Provider store={store}>
@@ -23,6 +33,7 @@ const App = () => (
             <img src={logo} className="App-logo" alt="logo" />
           </Link>
         </header>
+        <Toggle />
         <Switch>
           <Route exact path="/" component={MoviesList} />
           <Route path="/:id" component={MovieDetail} />
